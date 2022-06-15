7f7b0e17cf843aeae2a1fe85ed4008e26b7a9ef5
Tran Ngoc Thanh <97892253+thanhbk113@users.noreply.github.com>
2022-06-15 21:29:05 +0700

    Field component

--- src/components/OldYoutobeForm.js
+++ src/components/OldYoutobeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
 function YoutobeForm() {
@@ -28,36 +28,21 @@ function YoutobeForm() {
       <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            {...formik.getFieldProps("name")}
-          />
+          <Field type="text" id="name" name="name" />
           {formik.touched.name && formik.errors.name && (
             <div className="error">{formik.errors.name}</div>
           )}
         </div>
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            {...formik.getFieldProps("email")}
-          />
+          <Field type="email" id="email" name="email" />
           {formik.touched.email && formik.errors.email && (
             <div className="error">{formik.errors.email}</div>
           )}
         </div>
         <div className="fprm-control">
           <label htmlFor="channel">Channel</label>
-          <input
-            type="text"
-            id="channel"
-            name="channel"
-            {...formik.getFieldProps("channel")}
-          />
+          <Field type="text" id="channel" name="channel" />
           {formik.touched.channel && formik.errors.channel && (
             <div className="error">{formik.errors.channel}</div>
           )}
